9afd0be6878e5992bba08fe486a989de52887886
Rajkumar25012002 <19tuee120@skct.edu.in>
2023-08-07 12:22:04 +0530

    v6

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ const server = app.listen(process.env.PORT, () => {
 })
 const io=new Server(server,{
     cors:{
-        origin:"http://localhost:3000"
+        origin:"https://rockys-chat-app.netlify.app"
     },
     credentials:true
 })
